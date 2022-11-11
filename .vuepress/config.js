3fd0a4d5996e12494945b0f607611709329aaed9
Michael Kaufmann <d00p@froxlor.org>
2022-11-11 12:59:15 +0100

    menu-adjustments, formatting of config.js
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,224 +1,227 @@
-import {defaultTheme, defineUserConfig} from 'vuepress'
-import {registerComponentsPlugin} from "@vuepress/plugin-register-components";
+import { defaultTheme, defineUserConfig } from 'vuepress'
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
 import { getDirname, path } from '@vuepress/utils'
-import {palettePlugin} from "@vuepress/plugin-palette";
-import {searchPlugin} from "@vuepress/plugin-search";
-import {gitPlugin} from "@vuepress/plugin-git";
+import { palettePlugin } from "@vuepress/plugin-palette";
+import { searchPlugin } from "@vuepress/plugin-search";
+import { gitPlugin } from "@vuepress/plugin-git";
 
 const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
-    lang: 'en-US',
-    title: 'Froxlor Documentation',
-    description: 'Froxlor is the lightweight server management software for your needs.',
+	lang: 'en-US',
+	title: 'Froxlor Documentation',
+	description: 'Froxlor is the lightweight server management software for your needs.',
 
-    dest: 'dist',
+	dest: 'dist',
 
-    plugins: [
-        registerComponentsPlugin({
-            componentsDir: path.resolve(__dirname, './components'),
-        }),
-        palettePlugin({ preset: 'sass' }),
-        searchPlugin({}),
-        gitPlugin({
-            contributors: false
-        }),
-    ],
+	plugins: [
+		registerComponentsPlugin({
+			componentsDir: path.resolve(__dirname, './components'),
+		}),
+		palettePlugin({ preset: 'sass' }),
+		searchPlugin({}),
+		gitPlugin({
+			contributors: false
+		}),
+	],
 
-    theme: defaultTheme({
-        logo: '/logo.png',
-        logoDark: '/logo_white.png',
+	theme: defaultTheme({
+		logo: '/logo.png',
+		logoDark: '/logo_white.png',
 
-        docsRepo: 'https://github.com/Froxlor/Documentation',
+		docsRepo: 'https://github.com/Froxlor/Documentation',
 
-        navbar: [
-            {
-                text: 'v2',
-                children: [
-                    {
-                        text: 'v2',
-                        link: '/v2/',
-                    },
-                    {
-                        text: 'v0.10',
-                        link: 'https://docs.froxlor.org/v0.10/',
-                    }
-                ],
-            },
-            {
-                text: 'Discord',
-                link: 'https://discord.froxlor.org',
-            },
-            {
-                text: 'GitHub',
-                children: [
-                    {
-                        text: 'Froxlor',
-                        link: 'https://github.com/Froxlor/Froxlor',
-                    },
-                    {
-                        text: 'Documentation',
-                        link: 'https://github.com/Froxlor/Documentation',
-                    }
-                ],
-            }
-        ],
+		navbar: [
+			{
+				text: 'v2',
+				children: [
+					{
+						text: 'v2',
+						link: '/v2/',
+					},
+					{
+						text: 'v0.10',
+						link: 'https://docs.froxlor.org/v0.10/',
+					}
+				],
+			},
+			{
+				text: 'Discord',
+				link: 'https://discord.froxlor.org',
+			},
+			{
+				text: 'GitHub',
+				children: [
+					{
+						text: 'Froxlor',
+						link: 'https://github.com/Froxlor/Froxlor',
+					},
+					{
+						text: 'Documentation',
+						link: 'https://github.com/Froxlor/Documentation',
+					}
+				],
+			}
+		],
 
-        sidebar: [
-            {
-                text: 'General',
-                link: '/v2/general/',
-                children: [
-                    {
-                        text: 'Installation',
-                        link: '/v2/general/installation/',
-                        children: [
-                            {
-                                text: 'Manual (tarball)',
-                                link: '/v2/general/installation/tarball.md'
-                            },
-                            {
-                                text: 'Debian/Ubuntu',
-                                link: '/v2/general/installation/apt-package.md'
-                            },
-                            {
-                                text: 'Git (developer)',
-                                link: '/v2/general/installation/source.md'
-                            },
-                        ],
-                    },
-                    {
-                        text: 'Update Guide',
-                        link: '/v2/general/update-guide.md'
-                    },
-                    {
-                        text: 'Migration Guide',
-                        link: '/v2/general/migration-guide.md'
-                    },
-                ],
-            },
-            {
-                text: 'Admin Guide',
-                link: '/v2/admin-guide/',
-                children: [
-                    {
-                        text: 'Configuration',
-                        link: '/v2/admin-guide/configuration/',
-                        children: [
-                            {
-                                text: 'PHP-FPM',
-                                link: '/v2/admin-guide/configuration/php-fpm/'
-                            },
-                            {
-                                text: 'FCGID',
-                                link: '/v2/admin-guide/configuration/fcgid/'
-                            }
-                        ]
-                    },
-                    {
-                        text: 'Settings',
-                        link: '/v2/admin-guide/settings/'
-                    },
-                    {
-                        text: 'Resources',
-                        link: '/v2/admin-guide/resources/',
-                        children: [
-                            {
-                                text: 'IPs & Ports',
-                                link: '/v2/admin-guide/resources/ips-and-ports/'
-                            },
-                            {
-                                text: 'Admins / Resellers',
-                                link: '/v2/admin-guide/resources/admins-resellers/'
-                            },
-                            {
-                                text: 'Customers',
-                                link: '/v2/admin-guide/resources/customers/'
-                            },
-                            {
-                                text: 'Domains',
-                                link: '/v2/admin-guide/resources/domains/'
-                            },
-                            {
-                                text: 'MySQL Servers',
-                                link: '/v2/admin-guide/resources/mysql-servers/'
-                            },
-                            {
-                                text: 'Hosting Plans',
-                                link: '/v2/admin-guide/resources/hosting-plans/'
-                            },
-                            {
-                                text: 'SSL Certificates',
-                                link: '/v2/admin-guide/resources/ssl-certificates/'
-                            }
-                        ]
-                    },
-                    {
-                        text: 'PHP Configurations',
-                        link: '/v2/admin-guide/php-versions-and-configuration/'
-                    },
-                    {
-                        text: 'Domain import',
-                        link: '/v2/admin-guide/domain-import/'
-                    },
-                    {
-                        text: 'Froxlor console scripts (CLI)',
-                        link: '/v2/admin-guide/cli-scripts/'
-                    },
-                ]
-            },
-            {
-                text: 'User Guide',
-                link: '/v2/user-guide/',
-                children: [
-                    {
-                        text: 'E-Mails',
-                        link: '/v2/user-guide/emails/',
-                        children: [
-                            {
-                                text: 'E-Mail Accounts',
-                                link: '/v2/user-guide/emails/accounts/'
-                            },
-                            {
-                                text: 'E-Mail Domains',
-                                link: '/v2/user-guide/emails/domains/'
-                            },
-                        ]
-                    },
-                    {
-                        text: 'Databases',
-                        link: '/v2/user-guide/databases/'
-                    },
-                    {
-                        text: 'Domains',
-                        link: '/v2/user-guide/domains/'
-                    },
-                    {
-                        text: 'FTP Accounts',
-                        link: '/v2/user-guide/ftp-accounts/'
-                    },
-                    {
-                        text: 'Extras',
-                        link: '/v2/user-guide/extras/'
-                    },
-                    {
-                        text: 'Traffic',
-                        link: '/v2/user-guide/traffic/'
-                    },
-                ]
-            },
-            {
-                text: 'API Guide',
-                link: '/v2/api-guide/'
-            },
-            {
-                text: 'Security',
-                link: '/v2/security/'
-            },
-            {
-                text: 'Contribution',
-                link: '/v2/contribution/'
-            },
-        ],
-    }),
+		sidebar: [
+			{
+				text: '1. General',
+				link: '/v2/general/',
+				collapsible: true,
+				children: [
+					{
+						text: 'Installation',
+						link: '/v2/general/installation/',
+						children: [
+							{
+								text: 'Manual (tarball)',
+								link: '/v2/general/installation/tarball.md'
+							},
+							{
+								text: 'Debian/Ubuntu',
+								link: '/v2/general/installation/apt-package.md'
+							},
+							{
+								text: 'Git (developer)',
+								link: '/v2/general/installation/source.md'
+							},
+						],
+					},
+					{
+						text: 'Update Guide',
+						link: '/v2/general/update-guide.md'
+					},
+					{
+						text: 'Migration Guide',
+						link: '/v2/general/migration-guide.md'
+					},
+				],
+			},
+			{
+				text: '2. Admin Guide',
+				link: '/v2/admin-guide/',
+				collapsible: true,
+				children: [
+					{
+						text: 'Configuration',
+						link: '/v2/admin-guide/configuration/',
+						children: [
+							{
+								text: 'PHP-FPM',
+								link: '/v2/admin-guide/configuration/php-fpm/'
+							},
+							{
+								text: 'FCGID',
+								link: '/v2/admin-guide/configuration/fcgid/'
+							}
+						]
+					},
+					{
+						text: 'Settings',
+						link: '/v2/admin-guide/settings/'
+					},
+					{
+						text: 'Resources',
+						link: '/v2/admin-guide/resources/',
+						children: [
+							{
+								text: 'IPs & Ports',
+								link: '/v2/admin-guide/resources/ips-and-ports/'
+							},
+							{
+								text: 'Admins / Resellers',
+								link: '/v2/admin-guide/resources/admins-resellers/'
+							},
+							{
+								text: 'Customers',
+								link: '/v2/admin-guide/resources/customers/'
+							},
+							{
+								text: 'Domains',
+								link: '/v2/admin-guide/resources/domains/'
+							},
+							{
+								text: 'MySQL Servers',
+								link: '/v2/admin-guide/resources/mysql-servers/'
+							},
+							{
+								text: 'Hosting Plans',
+								link: '/v2/admin-guide/resources/hosting-plans/'
+							},
+							{
+								text: 'SSL Certificates',
+								link: '/v2/admin-guide/resources/ssl-certificates/'
+							}
+						]
+					},
+					{
+						text: 'PHP Configurations',
+						link: '/v2/admin-guide/php-versions-and-configuration/'
+					},
+					{
+						text: 'Domain import',
+						link: '/v2/admin-guide/domain-import/'
+					},
+					{
+						text: 'Froxlor console scripts (CLI)',
+						link: '/v2/admin-guide/cli-scripts/'
+					},
+				]
+			},
+			{
+				text: '3. User Guide',
+				link: '/v2/user-guide/',
+				collapsible: true,
+				children: [
+					{
+						text: 'E-Mails',
+						link: '/v2/user-guide/emails/',
+						children: [
+							{
+								text: 'E-Mail Accounts',
+								link: '/v2/user-guide/emails/accounts/'
+							},
+							{
+								text: 'E-Mail Domains',
+								link: '/v2/user-guide/emails/domains/'
+							},
+						]
+					},
+					{
+						text: 'Databases',
+						link: '/v2/user-guide/databases/'
+					},
+					{
+						text: 'Domains',
+						link: '/v2/user-guide/domains/'
+					},
+					{
+						text: 'FTP Accounts',
+						link: '/v2/user-guide/ftp-accounts/'
+					},
+					{
+						text: 'Extras',
+						link: '/v2/user-guide/extras/'
+					},
+					{
+						text: 'Traffic',
+						link: '/v2/user-guide/traffic/'
+					},
+				]
+			},
+			{
+				text: '4. API Guide',
+				link: '/v2/api-guide/'
+			},
+			{
+				text: '5. Security',
+				link: '/v2/security/'
+			},
+			{
+				text: '6. Contribution',
+				link: '/v2/contribution/'
+			},
+		],
+	}),
 })
--- v2/general/README.md
+++ v2/general/README.md
@@ -1,1 +1,9 @@
 # General
+
+This section covers installation, update and migration of froxlor.
+
+## Guides
+
+* [Installation](installation)
+* [Update](update-guide)
+* [Migration](migration-guide)
